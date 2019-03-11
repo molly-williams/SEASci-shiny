450a0f5410d049e0c9bc515e6db89359b9b29a86
mollywilliams <mollylegate@gmail.com>
2019-03-10 18:08:49 -0700

    changes to app.r

--- SEASci/app.R
+++ SEASci/app.R
@@ -41,6 +41,16 @@ whalesdf2$month <- as.numeric(whalesdf2$month)
 new <- whalesdf2 %>% 
   filter(year > 1970)
 
+# Create separate df for each species to call on as a group 
+humpbacks <- new %>% 
+  filter(vernacularName == "Humpback Whale")
+
+grays <- new %>% 
+  filter(vernacularName == "Gray Whale")
+
+blues <- new %>% 
+  filter(vernacularName == "Blue Whale")
+
 whale_icon <- makeIcon(
   iconUrl = "https://cdn2.iconfinder.com/data/icons/funtime-animals-humans/60/004_004_whale_sea_ocean_animal_fountain-512.png",
   iconWidth = 30, iconHeight = 30,
@@ -53,17 +63,19 @@ whale_icon <- makeIcon(
 
 ui <- fluidPage(theme = shinytheme("cerulean"),
                 
-                h2("Endangered Cetacean Sightings 2013-2018"),
+                titlePanel("Endangered Cetacean Sightings 2013-2018"),
                 
                 sidebarLayout(
                   sidebarPanel(
                     
-                    selectInput(inputId = "year",
-                                label="Year:",
-                                selected = "2018",
-                                choices = sort(unique(new$year))),
+                    helpText("Visualize observations of three endangered whale species recorded on the California Coast from 2013-2018."),
                     
-                    sliderInput(inputId = "month",
+                    selectInput("year",
+                                "Year:",
+                                choices = sort(unique(new$year)),
+                                selected = "2018"),
+                    
+                    sliderInput("month",
                                 label="Month:",
                                 min = 1,
                                 max=12,
@@ -81,54 +93,69 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
   
   
 server <- function(input, output) {
-  
-  
+
+  points <- eventReactive(input$species, {cbind(rnorm(40)*2+13, rnorm(40) + 48)
+    }, ignoreNULL = FALSE)
+    
+    
   output$mymap <- renderLeaflet({  
+   
+#     map <- new %>% 
+#      filter(year==input$year)
+#      filter(month==input$month) %>% 
+#      filter(species==input$species)
     
+
     
-    map <- new %>% 
-      filter(year == input$year) %>% 
-      filter(month == input$month) %>% 
-      filter(vernacularName == input$species)
-    
-    
-    
-  leaflet(map) %>%
-    addTiles() %>%
-    addProviderTiles(providers$Esri.WorldImagery) %>% 
-    addMarkers(lng = new$lon, 
-               lat = new$lat,
-               popup = paste(sep="", 
-                             "<font size = 2 color = blue>", 
-                             "Scientific Name: ","</font>", 
-                             "<font size = 2 color = black>", 
-                             new$scientificName,"</font>", 
-                             "<br/>", 
-                             "<font size = 2 color = blue>", 
-                             "Total Sighted: ","</font>", 
-                             "<font size = 2 color = black>", 
-                             new$individualCount,"</font>",
-                             "<br/>", 
-                             "<font size = 2 color = blue>", 
-                             "Occurrence ID: ","</font>", 
-                             "<font size = 2 color = black>", 
-                             new$OccurenceID, "</font>",
-                             "<br/>", 
-                             "<font size = 2 color = blue>", 
-                             "Latitude: ","</font>", 
-                             "<font size = 2 color = black>", 
-                             new$lat, "</font>",
-                             "<br/>",
-                             "<font size = 2 color = blue>", 
-                             "Longitude: ","</font>", 
-                             "<font size = 2 color = black>", 
-                             new$lon, "</font>",
-                             "</b>"),
-               icon = whale_icon)
+leaflet(new) %>%
+  addProviderTiles(providers$Esri.WorldImagery) %>%
+  addMarkers(data = humpbacks, lng = new$lon, lat = new$lat, group = "Humpback Whale") %>% 
+  addMarkers(data = grays, lng = new$lon, lat = new$lat, group = "Gray Whale") %>%
+  addMarkers(data = blues, lng = new$lon, lat = new$lat, group = "Blue Whale") %>%
+  addLayersControl(
+          baseGroups = c("Humpback Whale", "Gray Whale", "Blue Whale"),
+          options = layersControlOptions(collapsed = FALSE))
+      
+      
+      
+      
+      
+      # addMarkers(lng = new$lon, 
+      #         lat = new$lat,
+       #        popup = paste(sep="", 
+        #                     "<font size = 2 color = blue>", 
+         #                    "Scientific Name: ","</font>", 
+          #                   "<font size = 2 color = black>", 
+           #                  new$scientificName,"</font>", 
+            #                 "<br/>", 
+             #                "<font size = 2 color = blue>", 
+              #               "Total Sighted: ","</font>", 
+               #              "<font size = 2 color = black>", 
+                #             new$individualCount,"</font>",
+                 #            "<br/>", 
+                  #           "<font size = 2 color = blue>", 
+                   #          "Occurrence ID: ","</font>", 
+                    #         "<font size = 2 color = black>", 
+                     #        new$OccurenceID, "</font>",
+                      #       "<br/>", 
+                       #      "<font size = 2 color = blue>", 
+                        #     "Latitude: ","</font>", 
+                         #    "<font size = 2 color = black>", 
+                          #   new$lat, "</font>",
+                           #  "<br/>",
+                            # "<font size = 2 color = blue>", 
+                             #"Longitude: ","</font>", 
+              #               "<font size = 2 color = black>", 
+               #              new$lon, "</font>",
+                #             "</b>"),
+        #       icon = whale_icon)
   
   })
   
+  observe({
+    proxy <- leafletProxy("mymap")
+  })
   
 }
 
-shinyApp(ui, server)
+shinyApp(ui = ui, server = server)

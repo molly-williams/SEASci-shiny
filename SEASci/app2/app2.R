12f67cd61914dc4d83c86488a20a8365972c5183
mollywilliams <mollylegate@gmail.com>
2019-03-11 22:53:50 -0700

    trying out leaflet instead of tmap

--- SEASci/app2/app2.R
+++ SEASci/app2/app2.R
@@ -1,14 +1,15 @@
-# Global
+
 library(shiny)
-library(leaflet)
 library(tidyverse)
+library(sf)
 library(shinythemes)
+library(tmap)
+library(leaflet)
 library(RColorBrewer)
 library(dbplyr)
 library(stringr)
 library(lubridate)
 library(dplyr)
-library(sf)
 library(sp)
 library(raster)
 library(rgeos)
@@ -18,6 +19,7 @@ library(crosstalk)
 
 # Process
 
+### Import and wrangle sighting data    
 whales<- read_csv("sp_obis_westcoast.csv")
 
 whalesdf <- as.data.frame(whales)
@@ -34,111 +36,203 @@ whalesdf2 <- whalesdf %>%
 
 # Isolate month and year from observation time stamp:
 
-whalesdf2$date_simple <- as.Date(whalesdf2$EventDate, format="%Y/%M/%D")
+whalesdf2$date <- as.Date(whalesdf2$EventDate, format="%Y/%M/%D")
 whalesdf2$year <- format(as.Date(whalesdf2$EventDate, format="%Y/%M/%d"),"%Y")
-whalesdf2$month <- format(as.Date(whalesdf2$date_simple, format="%Y/%m/%d"),"%m")
+whalesdf2$month <- format(as.Date(whalesdf2$date, format="%Y/%m/%d"),"%m")
 whalesdf2$year <- as.numeric(whalesdf2$year)
 whalesdf2$month <- as.numeric(whalesdf2$month)
 
+new <- whalesdf2 %>%
+    dplyr::select(vernacularName, scientificName, OccurenceID, individualCount, lat, lon, occurenceStatus, date, month, year) %>%
+    dplyr::filter(year > 1970) %>%
+    dplyr::filter(individualCount < 10)
+
+humps <- new %>%
+  dplyr::filter(vernacularName == "Humpback Whale")
+
+blues <- new %>% 
+  dplyr::filter(vernacularName == "Blue Whale")
+
+grays <- new %>% 
+  dplyr::filter(vernacularName == "Gray Whale")
+  
+
+write.csv(new, "newdata.csv")
+
+# Create shape file based on lat/longs:
+coordinates(new) <- ~lon+lat
+proj4string(new)<- CRS("++proj=longlat +datum=WGS84") # set coordinate system to WGS
+
+LL_coords <- spTransform(new,CRS("+proj=longlat"))
+raster::shapefile(LL_coords, "WhaleShapefile2.shp", overwrite=TRUE)
 
 
-new <- whalesdf2 %>% 
-    filter(year > 1970)
+whale_shp <- read_sf("WhaleShapefile2.shp") %>% 
+    rename("Whales Sighted" = indvdlC)
+
+hump_shp <- whale_shp %>% 
+    filter(vrnclrN == "Humpback Whale")
+
+blue_shp <- whale_shp %>% 
+    filter(vrnclrN == "Blue Whale")
+
+gray_shp <- whale_shp %>% 
+    filter(vrnclrN == "Gray Whale")
+
+whale_shp # check extents in output
+st_crs(whale_shp) # check projection; its WGS84
 
 
-# Define UI 
-ui <- dashboardPage(
+### Make whale icon for observation points
+whale_icon <- makeIcon(
+    iconUrl = "https://cdn2.iconfinder.com/data/icons/funtime-animals-humans/60/004_004_whale_sea_ocean_animal_fountain-512.png",
+    iconWidth = 30, iconHeight = 30,
+    iconAnchorX = 10, iconAnchorY = 10
     
+)
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+    theme = shinytheme("cerulean"),
+    # Application title
+    titlePanel("Endangered Cetacean Sightings"),
     
-    dashboardHeader(title = "Endangered Cetacean Sightings 2013-2018",titleWidth = 450),
-                    
-    
-    dashboardSidebar(
-        sidebarMenu(
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
             
-
-            selectInput(inputId = "year",
-                        label="Year:",
-                        selected = "2018",
-                        choices = sort(unique(new$year))),
-            
+            helpText("Visualize observations of three endangered whale species recorded along the California Coast by volunteer naturalists from 2013-2018."),
             
             sliderInput(inputId = "month",
-                        label="Month:",
+                        label = "Month:",
                         min = 1,
-                        max=12,
+                        max = 12,
                         value = c(1,12)),
             
-            checkboxGroupInput(inputId = "species", 
-                               label = "Species",
-                               choices = list("Blue Whale" = 1, "Gray Whale" = 2, "Humpback Whale" = 3),
-                               selected = 1)
+            selectInput(inputId = "year",
+                        label = "Year:",
+                        selected = "2018",
+                        choices = c(2013,2014,2015,2016,2017,2018))
             
-
+            #            checkboxGroupInput(inputId = "species", 
+            #                               label = "Species",
+            #                               choices = list("Blue Whale" = 1, "Gray Whale" = 2, "Humpback Whale" = 3),
+            #                               selected = 1)
             
-            
-        )),
-    
-    
-    body<-dashboardBody(
+        ), # close parenthesis for sidebarPanel
         
-        tabItems(
-            
-            
-            tabItem(tabName="cinms",
-                    fluidRow(
-                        box(
-                            title = "Whale Conservation Map",
-                            collapsible = TRUE,
-                            background = "blue",
-                            width = "100%",
-                            height = "1000px",
-                            leafletOutput("cinms")
-                        )))
-            
+        # Create tabs for the reactive map, data, and summary of the data
+        mainPanel(
+            tabsetPanel(
+                
+                #map
+                tabPanel("Whale Map", leafletOutput("map")),
+                
+                
+                #data
+                tabPanel("Data", dataTableOutput("table")),
+                
+                #summary
+                tabPanel("Summary", 
+                         textInput("txt", tags$h6("The Channel Island National Marine Sanctuary (CINMS) has been overseeing a citizen science project since the 1990s. 
+                               This initiative is known as the Channel Islands Natualists Corps, comprised of over 160 volunteers collecting data on marine life 
+                               in the Santa Barbara Channel (SBC). Initially, cetacean sightings were recorded on paper logs. Since 2013, volunteers input data directly 
+                              into the Whale Spotter Pro mobile application while aboard marine vessels, typically the Condor Express whale watching boat that ports
+                              in Santa Barbara, CA. Only trained CINC volunteers can access the Spotter Pro app, but another CINMS mobile application called Whale Alert 
+                              allows the general public to record cetacean sightings. This citizen collected information has been used to create on of the largest
+                              datasets on marine mammals in the SBC, and was even used by CINMS to move shipping lanes by one nautical mile to prevent whale ship strikes.")),
+                         verbatimTextOutput("summary"))
+                
+            )
         )
     )
 )
 
-
-
-
-
-
-
-
-
-
-server <- function(input, output, session) {
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    
+    w <- reactive({read.csv("newdata.csv")})
     
     
-    whale_icon <- makeIcon(
-        iconUrl = "https://cdn2.iconfinder.com/data/icons/funtime-animals-humans/60/004_004_whale_sea_ocean_animal_fountain-512.png",
-        iconWidth = 30, iconHeight = 30,
-        iconAnchorX = 10, iconAnchorY = 10)
+    # Creating the reactive output ('map')
     
-    output$mymap <- renderLeaflet({  
+    output$map <- renderLeaflet({
+        
+      hump_obs <- hump_shp %>%
+        filter(month == input$month) %>% 
+        filter(year == input$year)
+      
+      blue_obs <- blue_shp %>%
+        filter(month == input$month) %>% 
+        filter(year == input$year)
+      
+      gray_obs <- gray_shp %>%
+        filter(month == input$month) %>% 
+        filter(year == input$year)
+      
+      
+        leaflet(width = "100%", height = "800px") %>%
+            addProviderTiles("Esri.WorldImagery", group = "Esri.WorldImagery") %>%
+            addMarkers(data = hump_obs, lng = humps$lon, lat = humps$lat,
+                       popup = paste("Date: " = "date"),
+                       clusterOptions = markerClusterOptions(),
+                       group = "Humpback Whales") %>% 
+          addMarkers(data = blue_obs, lng = blues$lon, lat = blues$lat,
+                     popup = paste("Date: " = "date"),
+                     clusterOptions = markerClusterOptions(),
+                     group = "Blue Whales")
         
         
-        map <- new %>% 
-            filter(year==input$year) %>%
-            filter(month==input$month) %>% 
-            filter(species==input$species)
-        
+        #popup = paste(sep="", 
+                      #                     "<font size = 2 color = blue>", 
+                      #                    "Scientific Name: ","</font>", 
+                      #                   "<font size = 2 color = black>", 
+                      #                  new$scientificName,"</font>", 
+                      #                 "<br/>", 
+                      #                "<font size = 2 color = blue>", 
+                      #               "Total Sighted: ","</font>", 
+                      #              "<font size = 2 color = black>", 
+                      #             new$individualCount,"</font>",
+                      #            "<br/>", 
+                      #           "<font size = 2 color = blue>", 
+                      #          "Occurrence ID: ","</font>", 
+                      #         "<font size = 2 color = black>", 
+                      #        new$OccurenceID, "</font>",
+                      #       "<br/>", 
+                      #      "<font size = 2 color = blue>", 
+                      #     "Latitude: ","</font>", 
+                      #    "<font size = 2 color = black>", 
+                      #   new$lat, "</font>",
+                      #  "<br/>",
+                      # "<font size = 2 color = blue>", 
+                      #"Longitude: ","</font>", 
+                      #               "<font size = 2 color = black>", 
+                      #              new$lon, "</font>",
+                      #             "</b>"),
+                      #       icon = whale_icon)
             
-        })
+ #           tm_dots(size = "Whales Sighted", alpha = 0.5, col = "Whales Sighted", 
+  #                  popup.vars = c("Date: " = "date",
+   #                                "Scientific Name:  " = "scntfcN",
+    #                               "Total Sighted: " = "Whales Sighted", 
+     #                              "Occurrence ID:   " = "OccrnID"),
+      #              popup.format=list(OccrnID=list(format="s")))        
         
         
-        leaflet(map) %>%
-            addTiles() %>%
-            addProviderTiles(providers$Esri.NatGeoWorldMap) %>% 
-            addAwesomeMarkers(lat=new$lat,lng=new$lon)
-    }
+        #tmap_leaflet(whale_map)
+        #          overlayGroups = c("Humpback Whales", "Blue Whales")
+        
+    })
+    
+    #Data Table
+    output$table <- renderDataTable({ new<- w() })
+    
+    #Summary
+    output$summary <- renderText({ input$txt })
     
     
-    
-    
-    
+}
 
-
-shinyApp(ui, server)
+# Run the application 
+shinyApp(ui = ui, server = server)
